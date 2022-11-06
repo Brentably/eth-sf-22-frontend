1cbe4062ea408857c307013703cfaf25c003a1f0
Brentably <Brent@pabnet.com>
2022-11-05 17:46:04 -0700

    added connect wallet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { useEffect } from 'react';
 import { ethers } from "ethers";
+
+const connect = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  await provider.send("eth_requestAccounts", []);
+  const signer = provider.getSigner()
+  console.log('hi')
+}
 
 
 function App() {
+  
+  
+
   return (
     <div className="App">
-      yooo
+      <button onClick={connect}>Connect wallet</button>
     </div>
   );
 }
