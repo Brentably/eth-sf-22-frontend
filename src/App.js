263546ac6c6d8533573b96a054b4dd31945e04f6
Brentably <Brent@pabnet.com>
2022-11-05 18:19:29 -0700

    wallet shit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ethers } from "ethers";
 
-const connect = async () => {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  await provider.send("eth_requestAccounts", []);
-  const signer = provider.getSigner()
-  console.log('hi')
-}
 
 
 function App() {
+  const [signer, setSigner] = useState(null)
   
-  
+  const connect = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner()
+    setSigner(signer)
+    console.log(signer)
+  }
+
+
+
 
   return (
     <div className="App">
-      <button onClick={connect}>Connect wallet</button>
+      <button onClick={connect}>{signer ? "connected!" : "Connect Wallet"}</button>
+      <br/><br/><br/><br/><br/>
+      <button></button>
     </div>
   );
 }
